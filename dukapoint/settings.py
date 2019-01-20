6040483aa2778ee9ae45814ccc1e69ce333dab50
Otuoma Sanya <otuomasp@gmail.com>
2019-01-20 07:52:10 +0300

    TO DO: finish transfers module

--- dukapoint/settings.py
+++ dukapoint/settings.py
@@ -71,23 +71,12 @@ WSGI_APPLICATION = 'dukapoint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dukapoint',
-        'USER': 'root',
-        'PASSWORD': 'root101',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

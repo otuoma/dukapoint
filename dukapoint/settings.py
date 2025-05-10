2448bb808787e0e5b7bb763cb7a59e05651f5529
Otuoma Sanya <otuoma@users.noreply.github.com>
2025-05-10 21:48:27 +0300

    Update settings.py

--- dukapoint/settings.py
+++ dukapoint/settings.py
@@ -65,23 +65,23 @@ WSGI_APPLICATION = 'dukapoint.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dukapoint',
-        'USER': 'root',
-        'PASSWORD': 'rootpassword',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dukapoint',
+#         'USER': 'root',
+#         'PASSWORD': 'rootpassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
